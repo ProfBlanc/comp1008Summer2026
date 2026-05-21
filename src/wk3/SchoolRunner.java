4e27ad7b10998f260bb119e8aad1182ceae54f06
Prof Blanc <ben.blanc@georgebrown.ca>
2026-05-21 15:12:37 -0400

    Class Code May 21, 2026 @ 15h15

--- src/wk3/SchoolRunner.java
+++ src/wk3/SchoolRunner.java
@@ -10,8 +10,22 @@ public class SchoolRunner {
         //ask the user for a school
             //name, year founded, number of courses, population
        //create 3 objects using 3 unique constructor of School
+       String name = IO.readln("Enter school name: ");
+       String strYearFounded = IO.readln("Enter year founded: ");
+       String strNumCourses = IO.readln("Enter num courses: ");
+       String strPopulation = IO.readln("Enter population: ");
 
+       short yearFounded = Short.parseShort(strYearFounded);
+       byte numCourses = Byte.parseByte(strNumCourses);
+       int population = Integer.parseInt(strPopulation);
 
+       School s1 = new School(name, yearFounded, numCourses);
+       School s2 = new School(name);
+       School s3 = new School(numCourses, yearFounded, name);
+
+       s1.enrollStudents(population);
+       s2.enrollStudents(population * 2);
+       s3.enrollStudents(population * 3);
    }
    static void example1(){
        String text;
